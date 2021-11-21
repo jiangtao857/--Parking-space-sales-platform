a78f7745c717be350fab23a8410c4b4b692695c1
江涛 <1321707054@qq.com>
2021-11-21 20:45:43 +0800

    项目配置

---
+++ parkingSales-service/src/main/java/com/group18/parkingsalesservice/ParkingSalesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.group18.parkingsalesservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingSalesServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParkingSalesServiceApplication.class, args);
+    }
+
+}
---
+++ parkingSales-service/src/test/java/com/group18/parkingsalesservice/ParkingSalesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group18.parkingsalesservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingSalesServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
